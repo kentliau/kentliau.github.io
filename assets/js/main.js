8382642d6f8702b9e1247820f67e07fca20eff33
Kent Liau <kent.jr@me.com>
2014-09-28 09:41:13 +0800

    clean up code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,153 +1,148 @@
-var options = {
-  highlightToolbar: !1,
-  inlineCodeAutoSelect: !1,
-  fluidbox: !0
-};
+( function () {
+  'use strict';
 
-(function() {
-// jQuery highlight text plugins
-// It will select the text of first occurence
-// of jQuery collection of element
-jQuery.fn.selectText = function(){
-  var doc     = document,
-      element = this[0],
+  var options = {
+    highlightToolbar: false,
+    inlineCodeAutoSelect: false,
+    fluidbox: true
+  };
+
+  // jQuery highlight text plugins
+  // It will select the text of first occurence
+  // of jQuery collection of element
+  jQuery.fn.selectText = function () {
+    var doc = document,
+      element = this[ 0 ],
       range,
       selection;
 
-  if (doc.body.createTextRange) {
+    if ( doc.body.createTextRange ) {
       range = document.body.createTextRange();
-      range.moveToElementText(element);
+      range.moveToElementText( element );
       range.select();
-  } else if (window.getSelection) {
+    } else if ( window.getSelection ) {
       selection = window.getSelection();
       range = document.createRange();
-      range.selectNodeContents(element);
+      range.selectNodeContents( element );
       selection.removeAllRanges();
-      selection.addRange(range);
+      selection.addRange( range );
+    }
+
+    return selection.toString() || range.toString();
+  };
+
+  if ( options.fluidbox ) {
+    $( 'a' ).has( 'img:only-child' ).fluidbox( {
+      closeTrigger: [ {
+        selector: 'window',
+        event: 'scroll'
+      } ]
+    } );
   }
 
-  return selection.toString() || range.toString();
-};
-
-})();
-
-
-
-if( options.fluidbox ) {
-  $("a").has('img:only-child').fluidbox({
-    closeTrigger: [
-      {
-        selector: "window",
-        event: "scroll"
-      }
-    ]
-  });
-}
-
-if( options.highlightToolbar ) {
-  // Adds Toogle Line number to pygment highlight
-  var highlight = $('.highlight'),
-      dict      = {
-        'select-all' : 'Select All',
-        'select-all-tip' : 'Press to highlight the code',
-        'expand' : '&#x2190; Expand &#x2192;',
-        'expand-tip' : 'Press to expand',
-        'ecs-expand' : '&#x2192; Esc (&#9099;) &#x2190;',
-        'ecs-expand-tip' : 'Press to contract or Press Esc key (⎋)'
+  if ( options.highlightToolbar ) {
+    // Adds Toogle Line number to pygment highlight
+    var highlight = $( '.highlight' ),
+      dict = {
+        'select-all': 'Select All',
+        'select-all-tip': 'Press to highlight the code',
+        'expand': '&#x2190; Expand &#x2192;',
+        'expand-tip': 'Press to expand',
+        'ecs-expand': '&#x2192; Esc (&#9099;) &#x2190;',
+        'ecs-expand-tip': 'Press to contract or Press Esc key (⎋)'
       };
 
-  highlight
-    .has('.lineno')
-    .prepend( $('<input />',
-    {
-      type: 'checkbox',
-      class: 'toggle-lineno',
-      title: 'Press to toggle line numbers',
-      checked: false
-    }));
+    highlight
+      .has( '.lineno' )
+      .prepend( $( '<input />', {
+        type: 'checkbox',
+        class: 'toggle-lineno',
+        title: 'Press to toggle line numbers',
+        checked: false
+      } ) );
 
-  // Adds select text feature to pygment highlight
-  highlight
-    .prepend( $('<button />', {
-      class: 'select-all btn',
-      text: dict['select-all'],
-      title: dict['select-all-top']
-    }))
-    .on('click', 'button.select-all', function(e) {
-      var selectionText = $(this)
-                            .parent('.highlight')
-                            .find('pre')
-                            .selectText();
-      console.log(selectionText);
-    });
+    // Adds select text feature to pygment highlight
+    highlight
+      .prepend( $( '<button />', {
+        class: 'select-all btn',
+        text: dict[ 'select-all' ],
+        title: dict[ 'select-all-top' ]
+      } ) )
+      .on( 'click', 'button.select-all', function () {
+        var selectionText = $( this )
+          .parent( '.highlight' )
+          .find( 'pre' )
+          .selectText();
+        console.log( selectionText );
+      } );
 
 
-  // Adds expand Mode
-  highlight
-    .prepend( $('<button />', {
-      class: 'expand-mode btn',
-      html: dict['expand'],
-      title: dict['expand-tip'],
-      data: {
-        expandMode: false
-      }
-    }))
-    .on('click', 'button.expand-mode', function(e) {
-      e.stopPropagation();
-      $this = $(this);
+    // Adds expand Mode
+    highlight
+      .prepend( $( '<button />', {
+        class: 'expand-mode btn',
+        html: dict.expand,
+        title: dict[ 'expand-tip' ],
+        data: {
+          expandMode: false
+        }
+      } ) )
+      .on( 'click', 'button.expand-mode', function ( e ) {
+        e.stopPropagation();
+        var $this = $( this );
 
-      // toggle expand mode
-      $this
-        .data( 'expandMode', !$this.data('expandMode') )
-        .html( !!$this.data('expandMode') ? dict['ecs-expand'] : dict['expand'] )
-        .parent( '.highlight' )
-        .toggleClass( 'expand-mode' );
+        // toggle expand mode
+        $this
+          .data( 'expandMode', !$this.data( 'expandMode' ) )
+          .html( !!$this.data( 'expandMode' ) ? dict[ 'ecs-expand' ] : dict.expand )
+          .parent( '.highlight' )
+          .toggleClass( 'expand-mode' );
 
-      // toggle any other distraction stuff
-      $this
-        .parent('.highlight')
+        // toggle any other distraction stuff
+        $this
+          .parent( '.highlight' )
           .siblings()
-          .toggleClass('expand-mode-distraction');
+          .toggleClass( 'expand-mode-distraction' );
 
-      // change title tooltip
-      $this.prop('title', !!$this.data('expandMode') ? dict['ecs-expand-tip'] : dict['expand-tip']);
+        // change title tooltip
+        $this.prop( 'title', !!$this.data( 'expandMode' ) ? dict[ 'ecs-expand-tip' ] : dict[ 'expand-tip' ] );
 
-      // Attach one event listener to expand mode
-      if( !!$this.data('expandMode') ) {
-        // always attach an event if whenever the user click expand
-        $(document)
-          .on('keyup', function(e) {
-            e.stopPropagation();
-            console.log('keyup fired');
-            if( e.keyCode === 27 ) /*ECS key*/
-            {
-              // bring back all the distraction
-              $('.highlight.expand-mode')
-                .toggleClass('expand-mode')
+        // Attach one event listener to expand mode
+        if ( !!$this.data( 'expandMode' ) ) {
+          // always attach an event if whenever the user click expand
+          $( document )
+            .on( 'keyup', function ( e ) {
+              e.stopPropagation();
+              console.log( 'keyup fired' );
+              if ( e.keyCode === 27 ) /*ECS key*/ {
+                // bring back all the distraction
+                $( '.highlight.expand-mode' )
+                  .toggleClass( 'expand-mode' )
                   .siblings()
-                  .removeClass('expand-mode-distraction');
+                  .removeClass( 'expand-mode-distraction' );
 
-              $('button.expand-mode')
-                .data( 'expandMode', !$this.data('expandMode') )
-                .html( !!$this.data('expandMode') ? dict['ecs-expand'] : dict['expand'] );
+                $( 'button.expand-mode' )
+                  .data( 'expandMode', !$this.data( 'expandMode' ) )
+                  .html( !!$this.data( 'expandMode' ) ? dict[ 'ecs-expand' ] : dict.expand );
 
-              $(document).off('keyup');
+                $( document ).off( 'keyup' );
 
-              console.log('Esc pressed, event detached.');
-            }
-          });
-      } else {
-        // remove keyup event if user collapse by mouse click
-        $(document).off('keyup');
-        console.log('Collapse, keyup event removed');
-      }
-  });
-}
+                console.log( 'Esc pressed, event detached.' );
+              }
+            } );
+        } else {
+          // remove keyup event if user collapse by mouse click
+          $( document ).off( 'keyup' );
+          console.log( 'Collapse, keyup event removed' );
+        }
+      } );
+  }
 
-if(options.inlineCodeAutoSelect){
-  // Automatically select inline code when clicked
-  $(document).on('click', 'code:not(.highlight code)', function() {
-      var selectionText = $(this).selectText();
-      console.log(selectionText);
-    });
-}
+  if ( options.inlineCodeAutoSelect ) {
+    // Automatically select inline code when clicked
+    $( document ).on( 'click', 'code:not(.highlight code)', function () {
+      var selectionText = $( this ).selectText();
+      console.log( selectionText );
+    } );
+  }
+} )();
